384e1ea73d2bbb90a266974e16ebedb92fe18875
NguyenHuynh <nguyenhuynhdev@gmail.com>
2022-02-26 20:45:45 +0700

    Apply changes

--- main.dart.js
+++ main.dart.js
@@ -45522,14 +45522,14 @@ return A.a0x(this.r,s.b.al(0,r.gu(r)),B.dE)}}
 A.uP.prototype={
 K(a,b){return this.e.$2(b,this.f)}}
 A.xO.prototype={
-K(a,b){return new A.pm(B.AF,"Flutter Demo",A.a2i(null,B.p6),null)}}
+K(a,b){return new A.pm(B.AF,"Nguyen Huynh",A.a2i(null,B.p6),null)}}
 A.pz.prototype={
 aE(){return new A.ti(B.n)}}
 A.ti.prototype={
 KS(){this.aL(new A.UA(this))},
 K(a,b){var s,r=null
 this.a.toString
-s=A.a2f("Flutter Demo Home Page",r)
+s=A.a2f("Number123",r)
 return new A.qt(new A.nS(s,new A.DT(r,r,1/0,56),r),A.HL(A.a9q(A.a([B.Ga,A.a2f(""+this.d,A.bE(b).ae.e)],t.F),B.A4),r,r),A.a0L(B.wC,!1,this.gKR(),"Increment"),r)}}
 A.UA.prototype={
 $0(){++this.a.d},
